c653110a764fe09f935cf184716711da53d03a11
Sarah Jones <saj0083@auburn.edu>
2026-09-01 23:24:38 -0500

    Implement object creation and value testing
    Updated main function to create and test Starter_Object instances.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,28 @@ using namespace std;
 
 int main()
 {
-	//create a few starter objects
-	//add some values to test out your values methods
-	//print out all the values for each object to validate your code
+	// Create a few Starter_Object objects
+    Starter_Object object1;
+    Starter_Object object2;
+
+    // Add values to object1
+    object1.val1(10);
+    object1.val2(20);
+
+    // Add values to object2
+    object2.val1(30);
+    object2.val2(40);
+
+    // Print all values to validate the code
+    cout << "Object 1:" << endl;
+    cout << "val1 = " << object1.val1() << endl;
+    cout << "val2 = " << object1.val2() << endl;
+
+    cout << endl;
+
+    cout << "Object 2:" << endl;
+    cout << "val1 = " << object2.val1() << endl;
+    cout << "val2 = " << object2.val2() << endl;
+
+    return 0;
 }
